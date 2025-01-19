c592c056141c790f740273edfa738f7c13c5e254
pathumck <ckpathumck@gmail.com>
2025-01-20 02:33:45 +0530

    add react icons to nav links

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { MdDashboard} from "react-icons/md";
+import { FaSunPlantWilt, FaPlantWilt } from "react-icons/fa6";
+import { SiReadthedocs } from "react-icons/si";
+import { GiFarmer, GiFarmTractor, GiDigDug } from "react-icons/gi";
+import { FaUserFriends } from "react-icons/fa";
+import { RiLogoutCircleRLine } from "react-icons/ri";
 
 function NavBar() {
   return (
       <>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-            <img src="public/green shadow.png" alt="" width= "200" />
+            <img src="public/green shadow.png" alt="" width= "170" />
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavDropdown">
               <ul className="navbar-nav ms-auto">
                 <li className="nav-item">
-                  <Link className="nav-link" aria-current="page" to="/dashboard">Dashboard</Link>
+                  <Link className="nav-link" aria-current="page" to="/dashboard"><MdDashboard className="me-1" />Dashboard</Link>
                 </li>
                 <li className="nav-item dropdown">
                   <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <FaSunPlantWilt className="me-1" />
                     Fields
                   </Link>
                   <ul className="dropdown-menu">
@@ -26,13 +33,18 @@ function NavBar() {
                   </ul>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/crops">Crops</Link>
+                  <Link className="nav-link" to="/crops">
+                  <FaPlantWilt className="me-1" />
+                  Crops</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/staff">Staff</Link>
+                  <Link className="nav-link" to="/staff">
+                  <GiFarmer className="me-1" />
+                  Staff</Link>
                 </li>
                 <li className="nav-item dropdown">
                   <Link className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <SiReadthedocs className="me-1" />
                     Logs
                   </Link>
                   <ul className="dropdown-menu">
@@ -41,16 +53,24 @@ function NavBar() {
                   </ul>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/vehicles">Vehicles</Link>
+                  <Link className="nav-link" to="/vehicles">
+                  <GiFarmTractor className="me-1" />
+                  Vehicles</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/equipments">Equipments</Link>
+                  <Link className="nav-link" to="/equipments">
+                  <GiDigDug className="me-1" />
+                  Equipments</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/users">Users</Link>
+                  <Link className="nav-link" to="/users">
+                  <FaUserFriends className="me-1" />
+                  Users</Link>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="#">Logout</Link>
+                  <Link className="nav-link" to="#">
+                  <RiLogoutCircleRLine className="me-1" />
+                  Logout</Link>
                 </li>
               </ul>
             </div>
