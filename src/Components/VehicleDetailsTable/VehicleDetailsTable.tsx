8f7ec1be0211cf986fba64b09a78920f7ec2260b
pathumck <ckpathumck@gmail.com>
2025-01-20 23:08:12 +0530

    create vehicle details table component"

---
+++ src/Components/VehicleDetailsTable/VehicleDetailsTable.tsx
@@ -0,0 +1,95 @@
+import React from 'react'
+
+function VehicleDetailsTable() {
+  return (
+    <>
+      <div className='container-fluid'>
+        <table className="table mt-5">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Vehicle Code</th>
+              <th scope="col">Number</th>
+              <th scope="col">Category</th>
+              <th scope="col">Fuel Type</th>
+              <th scope="col">Remarks</th>
+              <th scope="col">Status</th>
+              <th scope="col">Staff Id</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
+}
+
+export default VehicleDetailsTable
--- src/Pages/Vehicles.tsx
+++ src/Pages/Vehicles.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import PageTitle from '../Components/PageTitle/PageTitle'
 import AddAndSearch from '../Components/AddAndSearch/AddAndSearch'
 import { GiFarmTractor } from 'react-icons/gi'
+import VehicleDetailsTable from '../Components/VehicleDetailsTable/VehicleDetailsTable'
 
 function Vehicles() {
   return (
     <>
       <PageTitle title = "Vehicles" icon = {<GiFarmTractor className='align-baseline' size={22} />}/>
       <AddAndSearch btntext = "vehicle" />
+      <VehicleDetailsTable />
     </>
   )
 }
