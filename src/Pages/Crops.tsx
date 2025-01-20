b34ea068f573faf6a3652c2641467742c38fd90a
pathumck <ckpathumck@gmail.com>
2025-01-20 22:40:12 +0530

    create crop details table component"

--- src/Pages/Crops.tsx
+++ src/Pages/Crops.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import PageTitle from '../Components/PageTitle/PageTitle'
 import { FaPlantWilt } from 'react-icons/fa6'
 import AddAndSearch from '../Components/AddAndSearch/AddAndSearch'
+import CropDetailsTable from '../Components/CropDetailsTable/CropDetailsTable'
 
 function Crops() {
   return (
     <>
       <PageTitle title = "Crops" icon = {<FaPlantWilt className='align-baseline' size={22} />}/>
       <AddAndSearch btntext = "crop" />
+      <CropDetailsTable />
     </>
   )
 }
