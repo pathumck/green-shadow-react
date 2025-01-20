f09c75f6336a7e7a4ad7875abfd0bd677b25af20
pathumck <ckpathumck@gmail.com>
2025-01-20 22:47:00 +0530

    create equipment details table component

--- src/Pages/Equipments.tsx
+++ src/Pages/Equipments.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import PageTitle from '../Components/PageTitle/PageTitle'
 import AddAndSearch from '../Components/AddAndSearch/AddAndSearch'
 import { GiDigDug } from 'react-icons/gi'
+import EquipmentDetailsTable from '../Components/EquipmentDetailsTable/EquipmentDetailsTable'
 
 function Equipments() {
   return (
     <>
       <PageTitle title = "Equipments" icon = {<GiDigDug className='align-baseline' size={22} />}/>
       <AddAndSearch btntext = "equipment" />
+      <EquipmentDetailsTable />
     </>
     
   )
