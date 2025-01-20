cb46957951714775da4a4251f5956a04ddff9bf5
pathumck <ckpathumck@gmail.com>
2025-01-20 22:26:56 +0530

    create field details table component

---
+++ src/Components/FieldDetailsTable/FieldDetailsTable.tsx
@@ -0,0 +1,88 @@
+import React from 'react'
+
+function FieldDetailsTable() {
+  return (
+    <>
+      <div className='container-fluid'>
+        <table className="table mt-5">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">FieldCode</th>
+              <th scope="col">Name</th>
+              <th scope="col">Location</th>
+              <th scope="col">Size</th>
+              <th scope="col">Image One</th>
+              <th scope="col">Image Two</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
+}
+
+export default FieldDetailsTable
--- src/Pages/FieldDetails.tsx
+++ src/Pages/FieldDetails.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import AddAndSearch from '../Components/AddAndSearch/AddAndSearch'
 import PageTitle from '../Components/PageTitle/PageTitle'
 import { FaSunPlantWilt } from 'react-icons/fa6'
+import FieldDetailsTable from '../Components/FieldDetailsTable/FieldDetailsTable'
 
 function FieldDetails() {
   return (
     <>
       <PageTitle title = "Field Details" icon = {<FaSunPlantWilt className='align-baseline' size={22} />}/>
       <AddAndSearch btntext = "field" />
+      <FieldDetailsTable />
     </>    
   )
 }
