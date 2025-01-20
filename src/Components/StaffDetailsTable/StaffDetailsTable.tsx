6f996bb4362e84ca068b07214adb291041e836e6
pathumck <ckpathumck@gmail.com>
2025-01-20 22:59:17 +0530

    create staff details table component

---
+++ src/Components/StaffDetailsTable/StaffDetailsTable.tsx
@@ -0,0 +1,117 @@
+import React from 'react'
+
+function StaffDetailsTable() {
+  return (
+    <>
+      <div className='container-fluid'>
+        <table className="table mt-5">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Staff Id</th>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+              <th scope="col">Birth Day</th>
+              <th scope="col">Gender</th>
+              <th scope="col">Phone</th>
+              <th scope="col">E mail</th>
+              <th scope="col">Address</th>
+              <th scope="col">Designation</th>
+              <th scope="col">Role</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+           
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
+}
+
+export default StaffDetailsTable
--- src/Pages/Staff.tsx
+++ src/Pages/Staff.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import PageTitle from '../Components/PageTitle/PageTitle'
 import AddAndSearch from '../Components/AddAndSearch/AddAndSearch'
 import { GiFarmer } from 'react-icons/gi'
+import StaffDetailsTable from '../Components/StaffDetailsTable/StaffDetailsTable'
 
 function Staff() {
   return (
     <>
       <PageTitle title = "Staff" icon = {<GiFarmer className='align-baseline' size={22} />}/>
       <AddAndSearch btntext = "staff" />
+      <StaffDetailsTable />
     </>
   )
 }
