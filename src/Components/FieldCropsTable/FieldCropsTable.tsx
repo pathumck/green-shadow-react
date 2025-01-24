520cbec79680e25e226b9f9c6dd277640c5eae79
pathumck <ckpathumck@gmail.com>
2025-01-24 14:52:18 +0530

    create & place field crops table

---
+++ src/Components/FieldCropsTable/FieldCropsTable.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import "./FieldCropsTable.css";
+
+function FieldCropsTable() {
+  return (
+    <>
+      <h6 className="sub-topic">Selected field </h6>
+      <div className="container-fluid table-wrapper">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Crop Code</th>
+              <th scope="col">Common Name</th>
+              <th scope="col">Scientific Name</th>
+              <th scope="col">Category</th>
+              <th scope="col">Season</th>
+              <th scope="col">Image</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+
+export default FieldCropsTable;
--- src/Pages/FieldCrops.tsx
+++ src/Pages/FieldCrops.tsx
@@ -1,26 +1,30 @@
-import React from 'react'
-import SelectaFieldContainer from '../Components/SelectaFieldContainer/SelectaFieldContainer'
-import PageTitle from '../Components/PageTitle/PageTitle'
-import { FaSunPlantWilt } from 'react-icons/fa6'
-import SelectaCropContainer from '../Components/SelectaCropContainer/SelectaCropContainer'
-import './css/FieldCrops.css'
+import React from "react";
+import SelectaFieldContainer from "../Components/SelectaFieldContainer/SelectaFieldContainer";
+import PageTitle from "../Components/PageTitle/PageTitle";
+import { FaSunPlantWilt } from "react-icons/fa6";
+import SelectaCropContainer from "../Components/SelectaCropContainer/SelectaCropContainer";
+import "./css/FieldCrops.css";
+import FieldCropsTable from "../Components/FieldCropsTable/FieldCropsTable";
 function FieldCrops() {
   return (
     <>
-      <PageTitle title = "Field's Crops" icon = {<FaSunPlantWilt className='align-baseline' size={22} />} />
-      <div className='row main-wrap-div gap-5'>
+      <PageTitle
+        title="Field's Crops"
+        icon={<FaSunPlantWilt className="align-baseline" size={22} />}
+      />
+      <div className="row main-wrap-div gap-5 mb-5">
         <div className="col-6 col-md-5 col-dev">
-        <SelectaFieldContainer />
+          <SelectaFieldContainer />
         </div>
         <div className="col-6 col-md-5 col-dev">
-        <SelectaCropContainer />
+          <SelectaCropContainer />
         </div>
-      
+        <div className="col-lg-12 col-md-11 col-sm-6 col-6 col-dev">
+          <FieldCropsTable />
+        </div>
       </div>
-      
     </>
-    
-  )
+  );
 }
 
-export default FieldCrops
+export default FieldCrops;
