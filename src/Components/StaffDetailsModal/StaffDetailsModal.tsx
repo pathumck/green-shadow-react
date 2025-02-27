de83ec44502c68a1f5e807f54a475a373302641b
pathumck <ckpathumck@gmail.com>
2025-02-27 22:45:09 +0530

    create save staff

--- src/Components/StaffDetailsModal/StaffDetailsModal.tsx
+++ src/Components/StaffDetailsModal/StaffDetailsModal.tsx
@@ -1,6 +1,50 @@
 import React from "react";
+import { useState } from "react";
+import { AppDispatch } from "../../redux/store/store";
+import { useDispatch } from "react-redux";
+import { createStaff } from "../../redux/slices/staffSlice";
+import Staff from "../../modals/Staff";
 
 function StaffDetailsModal(props: any) {
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [birthDay, setBirthDay] = useState<string>("");
+  const [gender, setGender] = useState<string>("");
+  const [phone, setPhone] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [address, setAddress] = useState<string>("");
+  const [designation, setDesignation] = useState<string>("");
+  const [role, setRole] = useState<string>("");
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleSubmit = async () => {
+    if (props.text.title === "Add") {
+      const newStaff = new Staff(
+        "",
+        firstName,
+        lastName,
+        birthDay,
+        gender,
+        phone,
+        email,
+        address,
+        designation,
+        role
+      );
+      console.log(newStaff);
+      await dispatch(createStaff(newStaff));
+      setFirstName("");
+      setLastName("");
+      setBirthDay("");
+      setGender("");
+      setPhone("");
+      setEmail("");
+      setAddress("");
+      setDesignation("");
+      setRole("");
+    }
+  };
   return (
     <>
       <div
@@ -30,41 +74,80 @@ function StaffDetailsModal(props: any) {
               <div className="row">
                 <div className="col-6">
                   <label>First Name</label>
-                  <input type="text" className="form-control" />
+                  <input
+                    value={firstName}
+                    type="text"
+                    className="form-control"
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
                 </div>
                 <div className="col-6">
                   <label>Last Name</label>
-                  <input type="text" className="form-control" />
+                  <input
+                    value={lastName}
+                    type="text"
+                    className="form-control"
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
                 </div>
                 <div className="col-6">
                   <label>Birth Day</label>
-                  <input type="date" className="form-control" />
+                  <input
+                    value={birthDay}
+                    type="date"
+                    className="form-control"
+                    onChange={(e) => setBirthDay(e.target.value)}
+                  />
                 </div>
                 <div className="col-6">
                   <label>Gender</label>
-                  <select className="form-select">
-                    <option selected value="1">
-                      Male
+                  <select
+                    value={gender}
+                    className="form-select"
+                    onChange={(e) => setGender(e.target.value)}
+                  >
+                    <option selected disabled value="">
+                      Select a gender
                     </option>
-                    <option value="2">Female</option>
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
                   </select>
                 </div>
                 <div className="col-6">
                   <label>Phone</label>
-                  <input type="text" className="form-control" />
+                  <input
+                    value={phone}
+                    type="text"
+                    className="form-control"
+                    onChange={(e) => setPhone(e.target.value)}
+                  />
                 </div>
                 <div className="col-6">
                   <label>E mail</label>
-                  <input type="text" className="form-control" />
+                  <input
+                    value={email}
+                    type="text"
+                    className="form-control"
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
                 </div>
                 <div className="col-6">
                   <label>Address</label>
-                  <input type="text" className="form-control" />
+                  <input
+                    value={address}
+                    type="text"
+                    className="form-control"
+                    onChange={(e) => setAddress(e.target.value)}
+                  />
                 </div>
                 <div className="col-6">
                   <label>Designation</label>
-                  <select className="form-select">
-                    <option selected disabled value="Select a designation">
+                  <select
+                    value={designation}
+                    className="form-select"
+                    onChange={(e) => setDesignation(e.target.value)}
+                  >
+                    <option selected disabled value="">
                       Select a designation
                     </option>
                     <option value="MANAGER">MANAGER</option>
@@ -81,8 +164,12 @@ function StaffDetailsModal(props: any) {
                 </div>
                 <div className="col-6">
                   <label>Role</label>
-                  <select className="form-select">
-                    <option value="Select a role" selected disabled>
+                  <select
+                    value={role}
+                    className="form-select"
+                    onChange={(e) => setRole(e.target.value)}
+                  >
+                    <option value="" selected disabled>
                       Select a role
                     </option>
                     <option value="MANAGER">MANAGER</option>
@@ -96,12 +183,16 @@ function StaffDetailsModal(props: any) {
             <div className="modal-footer">
               <button
                 type="button"
-                className="btn btn-secondary"
+                className="btn btn-secondary staff-modal-close"
                 data-bs-dismiss="modal"
               >
                 Close
               </button>
-              <button type="button" className="btn btn-primary">
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={handleSubmit}
+              >
                 {props.text.btnText}
               </button>
             </div>
--- src/redux/slices/staffSlice.tsx
+++ src/redux/slices/staffSlice.tsx
@@ -1,12 +1,46 @@
 import Staff from "../../modals/Staff";
 import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState: Staff[] = [];
+
+export const createStaff = createAsyncThunk<Staff, Staff>(
+  "staff/createStaff",
+  async (staff) => {
+    try {
+      const response = await axios.post("http://localhost:3000/staff", staff);
+      return response.data;
+    } catch (error) {
+      throw new Error("Failed to create staff");
+    }
+  }
+)
 
 export const staffSlice = createSlice({
   name: "staff",
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createStaff.fulfilled, (state, action) => {
+        state.push(action.payload);
+        console.log(action.payload);
+        alert("Staff created successfully");
+        const closeBtn = document.querySelector(
+          ".staff-modal-close"
+        ) as HTMLElement;
+        closeBtn.click();
+      })
+      .addCase(createStaff.rejected, (state, action) => {
+        console.log("Faild to create staff : ", action.error.message);
+        alert(action.error.message);
+      })
+      .addCase(createStaff.pending, () => {
+        console.log("Creating staff...");
+      });
+  }
 });
 
 export default staffSlice.reducer;
+
