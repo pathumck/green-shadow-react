10ee192107b77faa5333ea052ba62a3f275562ea
pathumck <ckpathumck@gmail.com>
2025-03-10 01:42:39 +0530

    update logo

--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -12,7 +12,7 @@ function NavBar() {
       <>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-            <img src="public/green shadow.png" alt="" width= "170" />
+            <img src="public/green-shadow.png" alt="" width= "110" />
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
