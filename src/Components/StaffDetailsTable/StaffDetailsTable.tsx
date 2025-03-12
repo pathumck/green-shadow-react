7c3cd4890493af51bcc4850130da6e75a871569a
pathumck <ckpathumck@gmail.com>
2025-03-12 15:50:33 +0530

    update table styles

--- src/Components/StaffDetailsTable/StaffDetailsTable.tsx
+++ src/Components/StaffDetailsTable/StaffDetailsTable.tsx
@@ -1,42 +1,73 @@
-import { AppDispatch, RootState } from '../../redux/store/store';
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { updateOrDelete } from '../../redux/slices/updateOrDeleteSlice';
-import { deleteStaff } from '../../redux/slices/staffSlice';
+import { AppDispatch, RootState } from "../../redux/store/store";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { updateOrDelete } from "../../redux/slices/updateOrDeleteSlice";
+import { deleteStaff } from "../../redux/slices/staffSlice";
 
 function StaffDetailsTable(props: any) {
   const staff = useSelector((state: RootState) => state.staff);
   const dispatch = useDispatch<AppDispatch>();
   const handleUpdateOrDelete = (id: string) => {
     dispatch(updateOrDelete(id));
-  }
+  };
 
   const handleDelete = (id: string) => {
     dispatch(deleteStaff(id));
-  }
+  };
   return (
     <>
-      <div className='container-fluid'>
-        <table className="table mt-5">
-          <thead>
-            <tr>
-              <th scope="col" className='text-center'>#</th>
-              <th scope="col" className='text-center'>Staff Id</th>
-              <th scope="col" className='text-center'>First Name</th>
-              <th scope="col" className='text-center'>Last Name</th>
-              <th scope="col" className='text-center'>Birth Day</th>
-              <th scope="col" className='text-center'>Gender</th>
-              <th scope="col" className='text-center'>Phone</th>
-              <th scope="col" className='text-center'>E mail</th>
-              <th scope="col" className='text-center'>Address</th>
-              <th scope="col" className='text-center'>Designation</th>
-              <th scope="col" className='text-center'>Role</th>
-              <th scope="col" className='text-center'>Action</th>
+      <div
+        className="container-fluid mt-5"
+        style={{ maxHeight: "320px", overflowY: "scroll" }}
+      >
+        <table className="table table-striped table-bordered">
+          <thead className="table-dark sticky-top">
+            <tr className="text-center" style={{ fontSize: "12px" }}>
+              <th scope="col">
+                #
+              </th>
+              <th scope="col">
+                Staff Id
+              </th>
+              <th scope="col">
+                First Name
+              </th>
+              <th scope="col">
+                Last Name
+              </th>
+              <th scope="col">
+                Birth Day
+              </th>
+              <th scope="col">
+                Gender
+              </th>
+              <th scope="col">
+                Phone
+              </th>
+              <th scope="col">
+                E mail
+              </th>
+              <th scope="col">
+                Address
+              </th>
+              <th scope="col">
+                Designation
+              </th>
+              <th scope="col">
+                Role
+              </th>
+              <th scope="col">
+                Action
+              </th>
             </tr>
           </thead>
           <tbody>
             {staff.map((staff, index) => (
-              <tr className='text-center' key={index}>
+              <tr
+                className="text-center fw-bolder"
+                key={index}
+                style={{ fontSize: "12px" }}
+              >
                 <th scope="row">{index + 1}</th>
                 <td>{staff.id}</td>
                 <td>{staff.firstName}</td>
@@ -49,14 +80,14 @@ function StaffDetailsTable(props: any) {
                 <td>{staff.designation}</td>
                 <td>{staff.role}</td>
                 <td>
-                <button
+                  <button
                     data-bs-toggle="modal"
                     data-bs-target={props.target}
                     onClick={() => {
                       props.setShowModal("Update");
                       handleUpdateOrDelete(staff.id);
                     }}
-                    className="btn btn-primary"
+                    className="btn btn-sm btn-primary"
                   >
                     Edit
                   </button>
@@ -65,7 +96,7 @@ function StaffDetailsTable(props: any) {
                       handleUpdateOrDelete(staff.id);
                       handleDelete(staff.id);
                     }}
-                    className="btn btn-danger mx-2 "
+                    className="btn btn-sm btn-danger mx-2 "
                   >
                     Delete
                   </button>
@@ -76,7 +107,7 @@ function StaffDetailsTable(props: any) {
         </table>
       </div>
     </>
-  )
+  );
 }
 
-export default StaffDetailsTable
+export default StaffDetailsTable;
