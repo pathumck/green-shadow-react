5d5284f53a3c0a021e6e19b3bea8b8e908d47b90
pathumck <ckpathumck@gmail.com>
2025-04-03 02:26:09 +0530

    update colors

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -39,8 +39,8 @@ function Login() {
         <div
           className="d-flex flex-column align-items-center justify-content-center rounded border-1 w-100 w-md-50 w-lg-25"
           style={{
-            boxShadow: "rgba(109, 183, 29, 0.5) 0px 5px 15px",
-            backgroundColor: "rgba(255, 255, 255, 0.4)",
+            boxShadow: "rgba(52, 53, 48, 0.5) 0px 5px 15px",
+            backgroundColor: "rgba(169, 218, 160, 0.4)",
             maxWidth: "350px",
           }}
         >
@@ -111,7 +111,16 @@ function Login() {
             )}
           </div>
 
-          <button onClick={handleLogin} className="btn btn-success w-75 mb-3">
+          <button
+            onClick={handleLogin}
+            className="btn btn-success w-75 mb-3"
+            style={{
+              background: "linear-gradient(90deg, #009688, #4caf50, #8bc34a)",
+              transition: "all 0.3s ease-in-out",
+            }}
+            onMouseOver={(e) => (e.currentTarget.style.opacity = "0.85")}
+            onMouseOut={(e) => (e.currentTarget.style.opacity = "1")}
+          >
             Login
           </button>
         </div>
